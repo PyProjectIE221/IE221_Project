e861534f275d1d922eb81087b6c935268194ff99
LuatTB <76168991+TranBinhLuatUIT@users.noreply.github.com>
2021-05-12 18:28:45 +0700

    Revert "ReadData.py"

--- python_ie221/ReadData.py
+++ python_ie221/ReadData.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-import pandas as pd
-from pandas.io.parsers import read_csv
-
-
 class ReadData:
     def __init__(self):
+        
         #leave this line below
         super().__init__()
-    
-    
-    def __init__(self,filepath):
-        self.data = read_csv(filepath=filepath)
-        save_data = self.data.to_csv('/IE221_Project/data')
