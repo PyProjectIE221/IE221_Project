59bbcbcce0d2b184e3ecf175e41a8f04e66fb4ea
minhquan1910 <nguyenminhquan1910@gmail.com>
2021-05-12 05:25:12 +0000

    Modified Processing.py

--- python_ie221/Processing.py
+++ python_ie221/Processing.py
@@ -21,10 +21,10 @@ class TFIDFVectorizer(TfidfVectorizer):
   def fit_transform(self,data):
     """ Change to tf-idf vector
     Args: 
-    data (list) : path to your data 
+      data (list) : path to your data 
     
     Returns:
-    sparse matrix of (n_samples, n_features)
+      sparse matrix of (n_samples, n_features)
     """
     vectorized = super().fit_transform(data)
     return vectorized
@@ -34,10 +34,10 @@ class Countvectorizer(CountVectorizer):
   def fit_transform(self,data):
     """ Change to tf-idf vector
     Args: 
-    data (list) : path to your data 
+      data (list) : path to your data 
     
     Returns:
-    sparse matrix of (n_samples, n_features)
+      sparse matrix of (n_samples, n_features)
     """
     vectorized = super().fit_transform(data)
     return vectorized
@@ -47,22 +47,22 @@ class RandomForest(RandomForestClassifier):
   def fit(self,X_train,y_train):
     """ Build a forest of trees from the training set (X, y).
     Args:
-    X_train : array-like, sparse matrix of shape (n_samples, n_features)
-    y_train : array-like of shape (n_samples,) or (n_samples, n_outputs)
+      X_train : array-like, sparse matrix of shape (n_samples, n_features)
+      y_train : array-like of shape (n_samples,) or (n_samples, n_outputs)
 
     Returns:
-    self: object
+      self: object
     """
     object = super().fit(X_train,y_train)
     return object
   def score(self,X_test,y_test):
     """ Return the mean accuracy on the given test data and labels.
     Args:
-    X_test : array-like of shape (n_samples, n_features)
-    y_test : array-like of shape (n_samples,) or (n_samples, n_outputs)
+      X_test : array-like of shape (n_samples, n_features)
+      y_test : array-like of shape (n_samples,) or (n_samples, n_outputs)
 
     Return:
-    Score : float
+      Score : float
     """
     predict_score = super().score(X_test,y_test)
     return predict_score
