42cbfcb175d71016068ca26f21e6fe83259069c1
ambitiousguy99 <huynhngocquan6499@email.com>
2021-05-13 16:39:03 +0700

    update read_data

--- python_ie221/ReadData.py
+++ python_ie221/ReadData.py
@@ -5,11 +5,29 @@ import pandas as pd
 from pandas.io.parsers import read_csv
 
 
-class ReadData:
+class ReadData(object):
+
     def __init__(self):
         #leave this line below
         super().__init__()
     
-    
-    def __init__(self,filepath):
-        self.data = read_csv(filepath=filepath)
+    def __init__(self, filepath):
+        self.data = pd.read_csv(filepath)
+
+    #def __init__(self,filename):
+     #   self._data = []
+      #  self.localdir = '/IE221_Project/data'
+       # self.filename = filename
+        #self.filepath = os.path.join(self.localdir,self.filename)
+        #self.read_data()
+    def read_data(self, filepath):
+        """Read data from file csv
+
+        Args: 
+            Filepath(string) : file data kiểu csv
+
+        Returns: 
+            dataFrame
+        """
+        self.data = pd.read_csv(filepath)
+        return self.data
