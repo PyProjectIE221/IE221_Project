ada55fe227576e4d3be6a6e7619aee49963a59d7
TranBinhLuatUIT <76168991+TranBinhLuatUIT@users.noreply.github.com>
2021-05-17 23:43:49 +0700

    modify Process
    design class

--- main_code.py
+++ main_code.py
@@ -1,2 +1,16 @@
 import python_ie221 as p1
+from python_ie221.Processing import *
 
+dt = p1.ReadData('data\Combined_News_DJIA.csv')
+
+pre = p1.PreProcessing()
+
+pre.fully_preprocess(dt.data)
+
+x_train = TFIDFVectorizer(max_features=10000).fit_transform(pre.x_train)
+x_test = TFIDFVectorizer(max_features=10000).fit_transform(pre.x_test)
+
+proces = Processing(x_train, x_test, pre.y_train, pre.y_test)
+
+print(proces.RandomForest.score)
+print(proces.KNNClassifier.score)
