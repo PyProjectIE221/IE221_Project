f321d9074cc87c592479fccfc5b21fbee5890e7d
TranBinhLuatUIT <76168991+TranBinhLuatUIT@users.noreply.github.com>
2021-05-18 07:53:20 +0700

    modify process

--- main_code.py
+++ main_code.py
@@ -10,7 +10,14 @@ pre.fully_preprocess(dt.data)
 x_train = TFIDFVectorizer(max_features=10000).fit_transform(pre.x_train)
 x_test = TFIDFVectorizer(max_features=10000).fit_transform(pre.x_test)
 
-proces = Processing(x_train, x_test, pre.y_train, pre.y_test)
+a = KNNClassifier(x_train,pre.y_train, x_test, pre.y_test, n_neighbors=10)
 
-print(proces.RandomForest.score)
-print(proces.KNNClassifier.score)
+b = KNNClassifier(x_train,pre.y_train, x_test, pre.y_test, n_neighbors=3)
+
+a.knn_process()
+
+b.knn_process()
+
+print(a.score)
+
+print(b.score)
